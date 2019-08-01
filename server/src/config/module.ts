6735d07ecd938b74f460b15821f4680d64259a0c
Leonardo Merza <ljmerza@gmail.com>
2019-08-01 15:52:04 -0400

    automated commit 01/08/2019 15:52:04

--- server/src/config/module.ts
+++ server/src/config/module.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { ConfigService } from './config.service';
 
 
-const envFile = path.resolve(__dirname, '..', '..', 'env', `${process.env.NODE_ENV || 'development'}.env`);
+const envFile = path.resolve(__dirname, '..', '..', 'env', `.env`);
 
 
 @Module({
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
   const port = process.env.PORT || 3001;
   console.log(`listening on port ${port}`)
+  
   await app.listen(port);
 }
 
